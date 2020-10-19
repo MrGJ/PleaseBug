021231bf6c4e23c78b2438a88f191aef99574af1
KYFA02 <mnico3343@gmail.com>
2020-10-19 23:02:00 +1100

    Movement and Interaction in test scene

---
+++ BugTheRPG/Assets/OurAssets/Scripts/GridPrototypes/GridMovementMKII.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovementMKII : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            MovePlayer(Vector3.forward);
+        }  
+        if (Input.GetKey(KeyCode.S))
+        {
+            MovePlayer(Vector3.back);
+        }  
+        if (Input.GetKey(KeyCode.A))
+        {
+            MovePlayer(Vector3.left);
+        }  
+        if (Input.GetKey(KeyCode.D))
+        {
+            MovePlayer(Vector3.right);
+        }  
+        
+
+      
+    }
+
+    public void MovePlayer(Vector3 direction)
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+}
