7a53f408ce3520d7a1b0e07264302724141ec26b
HosekiAnankos <jaspervonriegen@gmail.com>
2020-11-16 21:24:21 +1100

    Fiddling with the Inventory

---
+++ BugTheRPG/Assets/Scripts/BMarketInv.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BMarketInv : InventoryRetry
+{
+    //public List<Items> bMarketInv = new List<Items>();
+
+
+    ////Adds item to item list(needs id functionality)
+    //public override bool Add(Items item)
+    //{
+    //    if (!item.isDefault)
+    //    {
+    //        if (bMarketInv.Count >= baseSpace)
+    //        {
+    //            Debug.Log("SHOPSTANDSILLEGAL");
+    //            return false;
+    //        }
+    //        bMarketInv.Add(item);
+    //        onItemChangedRingADing.Invoke();
+    //    }
+    //    return true;
+    //}
+
+    ////Deletes item from item list
+    //public override void Delete(Items item)
+    //{
+    //    bMarketInv.Remove(item);
+    //    onItemChangedRingADing.Invoke();
+    //}
+}
