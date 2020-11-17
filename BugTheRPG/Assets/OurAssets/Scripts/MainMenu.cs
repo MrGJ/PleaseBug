1af832c5a9edab66a47226279c1ed32306406a06
Haiden Burns <haidenpburns20@gmail.com>
2020-11-17 11:02:50 +1100

    Animation for Menu

--- BugTheRPG/Assets/OurAssets/Scripts/MainMenu.cs
+++ BugTheRPG/Assets/OurAssets/Scripts/MainMenu.cs
@@ -5,19 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update 
+    public GameObject overworldCamera;
+    public GameObject mainCamera;
+    public GameObject canvas;
+
+    public Animator startAnim;
+    public Animator exitAnim;
+
+    public bool enter;
+    public bool exit;
+
     void Start()
     {
+        overworldCamera.SetActive(false);
+
+        startAnim.enabled = false;
+        exitAnim.enabled = false;
+
         Cursor.lockState = CursorLockMode.Confined;
     }
 
-    public void StartButton()
+    void Update()
     {
-        SceneManager.LoadScene("Garden", LoadSceneMode.Single);
+        if (enter == true)
+        {
+            overworldCamera.SetActive(true);
+            mainCamera.SetActive(false);
+        }
+        else if (exit == true)
+        {
+            Application.Quit();
+        }
     }
 
-    public void ExitButton()
+    public void StartButtonStart()
     {
-        Application.Quit();
+        canvas.SetActive(false);
+        startAnim.enabled = true;
+    }
+
+    public void ExitButtonStart()
+    {
+        canvas.SetActive(false);
+        exitAnim.enabled = true;
     }
 }
---
+++ BugTheRPG/Assets/Scripts/Exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public MainMenu script;
+
+    public void ExitAnim()
+    {
+        script.exit = true;
+    }
+}
