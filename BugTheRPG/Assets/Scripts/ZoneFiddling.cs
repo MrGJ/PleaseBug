555249fc79f50bdca22ec29fba4b676913f97308
HosekiAnankos <jaspervonriegen@gmail.com>
2020-11-24 23:43:12 +1100

    Battle Fiddling

---
+++ BugTheRPG/Assets/Scripts/ZoneFiddling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneFiddling : MonoBehaviour
+{
+    public EncounterTwisting twisties;
+    public BattleSystemWLevelling battleBits;
+
+    public GameObject zoneSandCam, zonePoolCam, zoneTutCam, zoneLineCam, zoneTreeCam, zonePondCam, zoneBossCam;
+    public Transform[] partyOnePlatform, partyTwoPlatform, partyThreePlatform, partyFourSandPlatform, enemyPlatform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
